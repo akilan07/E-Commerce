ed179c73c8f2d63a262b70d52dc4449e925e8aea
akilan07r <96283219+akilan07r@users.noreply.github.com>
2022-03-13 14:29:33 +0530

    base code

---
+++ src/main/java/com/akitech/SpringBooECommerce/SpringBooECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.akitech.SpringBooECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akitech/SpringBooECommerce/SpringBooECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akitech.SpringBooECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBooECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
